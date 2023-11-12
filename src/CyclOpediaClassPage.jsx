b8f76566e4f95a8847c9e0e5928f60071da565ed
Phuoc Phan <21520405@gm.uit.edu.vn>
2023-11-12 13:57:49 +0700

    initialize lifecycle (Mount, Update and UnMount Component)

--- src/CyclOpediaClassPage.jsx
+++ src/CyclOpediaClassPage.jsx
@@ -1,60 +1,110 @@
 import React from "react";
 import { getRandomUser } from "./Utility/Api";
+import Instructor from "./Instructor";
 
 class CyclOpediaClassPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
+    this.state = JSON.parse(localStorage.getItem("cyclopediaState")) || {
       instructor: undefined,
       studentList: [],
       studentCount: 0,
       hideInstructor: false,
+      inputName: "",
+      inputFeedback: "",
     };
   }
 
   componentDidMount = async () => {
     console.log("Component Did Mount");
-    const resposne = await getRandomUser();
-    console.log(resposne);
-    this.setState((prevState) => {
-      return {
-        instructor: {
-          name: resposne.data.first_name + " " + resposne.data.last_name,
-          email: resposne.data.email,
-          phone: resposne.data.phone_number,
-        },
-      };
-    });
+    if(JSON.parse(localStorage.getItem("cyclopediaState"))) {
+      // this.setState(JSON.parse(localStorage.getItem("cyclopediaState")));
+    }
+    else {
+      const resposne = await getRandomUser();
+      console.log(resposne);
+      this.setState((prevState) => {
+        return {
+          instructor: {
+            name: resposne.data.first_name + " " + resposne.data.last_name,
+            email: resposne.data.email,
+            phone: resposne.data.phone_number,
+          },
+        };
+      });
+    }
   };
 
   componentDidUpdate() {
     console.log("Component Did Update");
+    localStorage.setItem("cyclopediaState", JSON.stringify(this.state));
   }
 
   componentWillUnmount() {
     console.log("Component Will Unmount");
   }
 
+  handleAddStudent = () => {
+    this.setState((prevState) => {
+      return {
+        studentCount: prevState.studentCount + 1,
+      };
+    });
+  };
+
+  handleRemoveAllStudents = () => {
+    this.setState(() => {
+      return {
+        studentCount: 0,
+      };
+    });
+  };
+
   render() {
     console.log("Render Component");
     return (
       <div>
         {this.state.instructor && (
-          <div className="p-3">
-            <span className="h4 text-success">Instructor</span>
-            <i className="bi bi-toggle-off btn btn-success btn-sm"></i>
-            <br />
-            Name: {this.state.instructor.name} <br />
-            Email: {this.state.instructor.email} <br />
-            Phone: {this.state.instructor.phone} <br />
-          </div>
+          <Instructor instructor={this.state.instructor}/>
         )}
+        <div className="p-3">
+          <span className="h4 text-success">Feedback</span>
+          <br />
+          <input
+            type="text"
+            placeholder="Name..."
+            value={this.state.inputName}
+            onChange={(e) => {
+              this.setState({ inputName: e.target.value });
+            }}
+          ></input>{" "}
+          Value: {this.state.inputName}
+          <br />
+          <textarea
+            placeholder="Feedback..."
+            value={this.state.inputFeedback}
+            onChange={(e) => {
+              this.setState({ inputFeedback: e.target.value });
+            }}
+          ></textarea>{" "}
+          Value: {this.state.inputFeedback}
+        </div>
         <div className="p-3">
           <span className="h4 text-success">Students</span> <br />
           <div>Student Count: {this.state.studentCount}</div>
-          <button className="btn btn-success btn-sm">Add Student</button>
+          <button
+            onClick={this.handleAddStudent}
+            className="btn btn-success btn-sm"
+          >
+            Add Student
+          </button>
           &nbsp;
-          <button className="btn btn-danger btn-sm">Remove Student</button>
+          <button
+            onClick={this.handleRemoveAllStudents}
+            className="btn btn-danger btn-sm"
+          >
+            Remove All Students
+          </button>
         </div>
       </div>
     );
---
+++ src/Instructor.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+class Instructor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setState = {};
+  }
+
+  componentDidMount() {
+    console.log("Mounted - Instructor");
+  }
+
+  componentDidUpdate() {
+    console.log("Updated - Instructor");
+  }
+
+  componentWillUnmount() {
+    console.log("UnMount - Instructor");
+  }
+
+  render() {
+    console.log("Render - Instructor");
+    return (
+      <div className="p-3">
+        <span className="h4 text-success">Instructor</span>
+        <i className="bi bi-toggle-off btn btn-success btn-sm"></i>
+        <br />
+        Name: {this.props.instructor.name} <br />
+        Email: {this.props.instructor.email} <br />
+        Phone: {this.props.instructor.phone} <br />
+      </div>
+    );
+  }
+}
+
+export default Instructor;
